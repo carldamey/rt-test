bf8ee86b0d50f653fc08594e970b9b72274515ce
carldamey <carldamey@gmail.com>
2024-06-05 21:01:30 -0700

    update html, export question bank

--- question_bank.js
+++ question_bank.js
@@ -1,4 +1,4 @@
-const quizData = [
+const questions = [
 	{
 		question:
 			"The medication prostacyclin is used for which of the following purposes?",
@@ -11089,3 +11089,7 @@ const quizData = [
 			"Periodic chest radiograph\n\n\nOf the options offered, the purpose of an IPPB treatment is to prevent post-operative complications, including consolidation of sputum in the alveoli. Monitoring pre and post breath sounds and obtaining a periodic chest x-ray is most helpful in assessing the effectiveness of the IPPB therapy.",
 	},
 ]
+
+module.exports = {
+  questions
+}
