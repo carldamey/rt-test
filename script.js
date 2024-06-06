343bb3c30f722253a695eae914129d799bd36c2e
carldamey <carldamey@gmail.com>
2024-06-05 21:18:51 -0700

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const testSectionForm = document.getElementById("test-section")
+
 let currentQuestion = 0
 const questionOrder = []
 const wrongQuestions = []
+console.log(questions)
+
+
+testSectionForm.onsubmit('() => console.log("sdlkjsa"')
+
+function 
